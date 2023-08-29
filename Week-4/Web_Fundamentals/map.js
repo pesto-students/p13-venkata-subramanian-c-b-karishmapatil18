108ad0c883fdf1b663ecd1f762888d397508bec5
karishmapatil18 <karishmapatil185@gmail.com>
2023-08-29 21:11:29 +0530

    Chnages Done In Assignment

--- Week-4/Web_Fundamentals/map.js
+++ Week-4/Web_Fundamentals/map.js
@@ -1,8 +1,23 @@
-function calcWordFrequencies()
-{
-    var res = prompt("Enter string")
-    const Array = res.split(" ");
-    console.log(Array);
-}
-
-calcWordFrequencies();
+function calcWordFrequencies() {
+    const map = new Map();
+  
+    var input = prompt("Enter something");
+  
+    const arr_input = input.split(" ");
+  
+    for( var word of arr_input){
+  
+      if (map.has(word)){
+        map.set(word, map.get(word) +1);
+      }
+      else{
+        map.set(word, 1);
+      }
+    }
+  
+    for (const [word, frequency] of map) {
+      console.log(word+" : "+frequency);
+    }
+  }
+  
+  calcWordFrequencies();
